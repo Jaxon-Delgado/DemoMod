2c96e89fe6ae72538c1baf517036ba67458fc80c
Jaxon-Delgado <jax@vt.edu>
2021-09-27 11:35:01 -0400

    Edit on ModID and mod names

--- src/main/java/com/jax/tutorial/Tutorial.java
+++ src/main/java/com/jax/tutorial/Tutorial.java
@@ -18,7 +18,7 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
+@Mod("tutorialmod")
 public class Tutorial
 {
     // Directly reference a log4j logger.
@@ -46,7 +46,7 @@ public class Tutorial
     private void enqueueIMC(final InterModEnqueueEvent event)
     {
         // some example code to dispatch IMC to another mod
-        InterModComms.sendTo("examplemod", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
+        InterModComms.sendTo("tutorialmod", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
     }
 
     private void processIMC(final InterModProcessEvent event)
