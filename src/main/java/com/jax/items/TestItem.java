9f1ef2c8e839cd88c502378babde12c403d0892b
Jaxon-Delgado <jax@vt.edu>
2021-10-07 14:08:35 -0400

    First item

---
+++ src/main/java/com/jax/items/TestItem.java
@@ -0,0 +1,11 @@
+package com.jax.items;
+
+import net.minecraft.world.item.Item;
+
+public class TestItem extends Item {
+
+
+    public TestItem(Properties properties) {
+        super(properties);
+    }
+}
--- src/main/java/com/jax/tutorial/Tutorial.java
+++ src/main/java/com/jax/tutorial/Tutorial.java
@@ -1,29 +1,24 @@
 package com.jax.tutorial;
 
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
+import com.jax.setup.Registration;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.fmlserverevents.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.stream.Collectors;
-
 @Mod(Tutorial.MODID)
-public class Tutorial
-{
+public class Tutorial {
     private static final Logger LOGGER = LogManager.getLogger();
     public static final String MODID = "tutorialmod";
+
     public Tutorial() {
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+
+        Registration.init();
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+        bus.addListener(this::setup);
         MinecraftForge.EVENT_BUS.register(this);
     }
 
